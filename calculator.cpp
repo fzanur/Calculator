c8c757cd8129777f51427856b05b3e047a610c10
Fiza Noor <fizanoor2005@gmail.com>
2025-07-11 00:08:16 +0500

    Add files via upload
    Upgraded calculator. This allows us to work with multiple numbers instead of just 2

--- calculator.cpp
+++ calculator.cpp
@@ -1,24 +1,67 @@
 #include <iostream>
+#include <sstream> //for using stringstream for converting string to float
 using namespace std;
 
-int main()
+main()
 {
-	int d1, d2, result;
+	float num, result=0;
 	char operand;
-	cout<<"--------------------------------------------------\n";
-	cout<<"WELCOME TO A SIMPLE CALCULATOR For Only 2 Digits\n";
-	cout<<"--------------------------------------------------\n";
-	cout<<"Enter the first digit: ";
-	cin>>d1;
-	cout<<"Enter the second digit ";
-	cin>>d2;
+	bool first=true;
+	cout<<"----------------------------------\n";
+	cout<<"WELCOME TO A SIMPLE CALCULATOR\n";
+	cout<<"----------------------------------\n";
+
 	cout<<"Enter the operand you want to use(/, *, +, -): ";
 	cin>>operand;
-	if(operand='/') result=d1/d2;
-	if(operand='*') result=d1*d2;
-	if(operand='+') result=d1+d2;
-	if(operand='-') result=d1-d2;
-	cout<<d1<<operand<<d2<<"="<<result;
-	return result;
 	
+	cout<<"Enter the digits one by one (press = to calculate): ";
+	while (true)
+	{
+		string input;
+		cin>>input; 
+		if(input=="=") break;
+		
+		stringstream ss(input);//converts string to float
+		ss>>num;	
+			
+		if(first) 
+		{
+			/*setting first number as result so the operation can be performed 
+			e.g; result has been initialized as zero so without this the first value will be zero instead of what user enters*/
+			result=num; 
+			first=false;//allows us to move to the next number
+		}
+		else//first=false
+		{
+			if(operand=='/') 
+			{
+				if (num==0)
+				{
+					cout<<"ERROR! Division with zero.";
+					return 1;
+				}
+				else
+				{
+					result/=num;
+				}
+				
+			}
+			
+			
+			else if(operand=='*') 
+			{
+				result*=num;
+			}
+			else if(operand=='+') result+=num;
+			else if(operand=='-') result-=num;
+			else
+			{
+				cout<<"Invalid operand..";
+				return 1;
+			}
+		}
+	
+	}
+	cout<<"Result: "<<result;	
+	return 0;
 }
